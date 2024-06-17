2df32abd4def3cf46f0ccd3bd6974a5d16a8ef2c
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-06-17 23:04:10 +0200

    all-set

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,10 @@ import { GlobeDemo } from "../components/GlobeDemo";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello</p>
-      <CardDemo />
+      {/* <CardDemo />
       <Tooltip />
       <PinCode />
-      <GlobeDemo />
+      <GlobeDemo /> */}
     </main>
   );
 }
