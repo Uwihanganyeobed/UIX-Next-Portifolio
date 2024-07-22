77c6f5a1e7454efb36e7b62296319f7146f841dc
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-22 17:10:18 +0200

    removing unwanted functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import { GridBg } from "@/components/GridBg";
 import { HeroDemoPar } from "@/components/HeroDemoPar";
 import { NavbarDemo } from "@/components/NavbarDemo";
 import { SparklesPreview } from "@/components/SparklesPreview";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
@@ -10,7 +8,6 @@ export default function Home() {
       <div className="max-w-1xl w-full">
         <NavbarDemo />
         <SparklesPreview />
-        <p>Hello</p>
       </div>
     </main>
   );
