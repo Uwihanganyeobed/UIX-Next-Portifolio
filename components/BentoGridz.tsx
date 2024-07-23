9642668254ec16a6f96f71bf3ffc1acde3d0c866
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-23 18:00:01 +0200

    skeleton

--- components/BentoGridz.tsx
+++ components/BentoGridz.tsx
@@ -194,7 +194,7 @@ const SkeletonFour = () => {
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="https://i.imgur.com/8h8FIqj.jpg"
           alt="avatar"
           height="100"
           width="100"
@@ -209,7 +209,7 @@ const SkeletonFour = () => {
       </motion.div>
       <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="https://i.imgur.com/8h8FIqj.jpg"
           alt="avatar"
           height="100"
           width="100"
@@ -227,7 +227,7 @@ const SkeletonFour = () => {
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="https://i.imgur.com/8h8FIqj.jpg"
           alt="avatar"
           height="100"
           width="100"
