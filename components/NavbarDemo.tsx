30c86e94346b41870e632c087ed45f4d7507f0bf
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-23 19:07:35 +0200

    all-set

--- components/NavbarDemo.tsx
+++ components/NavbarDemo.tsx
@@ -19,7 +19,7 @@ function Navbar({ className }: { className?: string }) {
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 ", className)}
     >
-      <Menu setActive={setActive} className="flex items-center">
+      <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
